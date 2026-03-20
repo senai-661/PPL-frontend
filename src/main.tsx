460126a9679f5d25a5e280380f0be6f0a816f111
PABLO ISAAC SPONCHIADO SILVA <pablo.sponchiado@portalsesisp.org.br>
2026-03-20 13:40:20 -0300

    feat: configura ThemeProvider global com persistência de tema

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./css/index.css";
+import { ThemeProvider } from 'next-themes';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './css/index.css';
 
-  createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <ThemeProvider
+    attribute="class"
+    defaultTheme="light"
+    enableSystem={false}
+    storageKey="openline-theme"
+    disableTransitionOnChange
+  >
+    <App />
+  </ThemeProvider>,
+);
   
